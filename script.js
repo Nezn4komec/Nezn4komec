1a2e7bc19ad7363c47e2fc28cdff2268254303e3
Nezn4komec <sixzar@mail.ru>
2019-10-06 21:50:03 +0500

    init

---
+++ script.js
@@ -0,0 +1,8 @@
+function serverx() {
+ var x = document.getElementById("serverclose");
+ x.style.display = "none";
+}
+function serverz() {
+ var x = document.getElementById("serverclose");
+ x.style.display = "block";
+}
