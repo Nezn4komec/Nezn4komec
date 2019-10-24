d05ca2e8a762ab82f6c5641221386e99d8c2a7a1
Nezn4komec <56114041+Nezn4komec@users.noreply.github.com>
2019-10-24 16:05:45 +0500

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,1 +1,1 @@
-document.body.innerHTML = "<div class='center'><center><strong><BIG>© 2019 - Developer by Moby ➔</strong><a href='https://www.youtube.com/channel/UC4Nf4e4e2yRjVnOhjdqFfew'> YouTube</a></div>"
+document.write("<div class='center'><center><strong><BIG>© 2019 - Developer by Moby ➔</strong><a href='https://www.youtube.com/channel/UC4Nf4e4e2yRjVnOhjdqFfew'> YouTube</a></div>");
